e00cbeea35591a6d235972e3c53c3b2cce529640
Mina Makram <mina.makram@pharos-solutions.de>
2018-02-17 16:05:13 +0200

    fix translations

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module BudgetApp
 
     # Where the I18n library should search for translation files
     I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
-
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # Whitelist locales available for the application
     I18n.available_locales = [:en, :ar]
 
