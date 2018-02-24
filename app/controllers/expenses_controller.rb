ee40cb8114620cd43d29440b94ad136313eb3e6c
Mina Makram <mina.makram@pharos-solutions.de>
2018-02-24 14:40:17 +0200

    order expenses by date

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,7 +1,7 @@
 class ExpensesController < ApplicationController
 
   def index
-    @expenses = Expense.where(user_id: current_user.id)
+    @expenses = Expense.where(user_id: current_user.id).order(:date)
     if params[:category_id].present?
       @category = Category.find params[:category_id]
       @expenses = @expenses.where category_id: params[:category_id]
