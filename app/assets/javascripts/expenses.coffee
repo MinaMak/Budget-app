fba65edea7946cc9a23e29984068cee6b20a3c1e
Mina Makram <mina.makram@pharos-solutions.de>
2018-03-04 00:58:28 +0200

    feature: add filter date to expenses

--- app/assets/javascripts/expenses.coffee
+++ app/assets/javascripts/expenses.coffee
@@ -1,25 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready ->
-  $('body').on 'change', '#category', ->
-    if $(this).val() != '0'
-      window.location = '/expenses?category_id=' + $(this).val()
-    else
-      window.location = '/expenses'
-
-  $('body').on 'change', '#locale', ->
-    if $(this).val() == 'en'
-      if window.location.href.indexOf('locale=') > 0
-        window.location = window.location.href.replace('locale=ar','locale=en')
-      else if window.location.href.indexOf('?') > 0
-        window.location += '&locale=en'
-      else
-        window.location += '?locale=en'
-    else if $(this).val() == 'ar'
-      if window.location.href.indexOf('locale=') > 0
-        window.location = window.location.href.replace('locale=en','locale=ar')
-      else if window.location.href.indexOf('?') > 0
-        window.location += '&locale=ar'
-      else
-        window.location += '?locale=ar'
--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,12 +1,18 @@
 class ExpensesController < ApplicationController
 
   def index
-    @sum_all_expenses = Expense.where(user_id: current_user.id).sum(:value)
     @expenses = Expense.where(user_id: current_user.id).order(:date).paginate(page: params[:page], per_page: 10)
-    if params[:category_id].present?
-      @category = Category.find params[:category_id]
-      @expenses = @expenses.where category_id: params[:category_id]
+    if params[:category].present? && (category = Category.find_by_id params[:category]).present?
+      @expenses = @expenses.where category: category
     end
+    if params[:from_date].present? && params[:to_date].present?
+      from_date = extract_date params[:from_date]
+      to_date = extract_date params[:to_date]
+      @expenses = @expenses.where 'date between ? and ?', from_date, to_date
+    else
+      @expenses = @expenses.where 'date between ? and ?', Date.today.beginning_of_month, Date.today.end_of_month
+    end
+    @sum_all_expenses = @expenses.sum :value
   end
 
   def new
@@ -45,6 +51,11 @@ class ExpensesController < ApplicationController
   end
 
   private
+
+  def extract_date(date)
+    date_result = date['(1i)'] + '-' + date['(2i)'] + '-' + date['(3i)']
+    date_result.to_date
+  end
 
   def expense_params
     params.require(:expense).permit(:name, :value, :date, :category_id)
