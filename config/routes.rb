8cd9a811dd64d6313272320fe1e1c5df367d5e4d
Mina Makram <mina.makram@pharos-solutions.de>
2018-03-03 23:20:01 +0200

    enhance views for authentication forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :expenses
   resources :categories
-  root 'welcome#index'
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+
 
   devise_for :users, controllers: {
       sessions: 'users/sessions',
